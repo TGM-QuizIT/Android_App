6464e69682afcb70734108540e47b97b47a5dd69
marcelturetschek <113130348+marcelturetschek@users.noreply.github.com>
2025-01-14 14:30:27 +0100

    hide bottombar for "login" and "quiz/{focusId}" routes

--- app/src/main/java/com/example/quizit_android_app/ui/MainScreen.kt
+++ app/src/main/java/com/example/quizit_android_app/ui/MainScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.quizit_android_app.navigation.AppNavGraph
 import com.example.quizit_android_app.navigation.BottomBar
@@ -15,13 +17,16 @@ import com.example.quizit_android_app.ui.login.LoginScreen
 fun MainScreen(
     viewModel: MainViewModel = hiltViewModel()
 ) {
-
-
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination?.route
     Scaffold(
-        bottomBar = { BottomBar(navController = navController) }
+        bottomBar = {
+            if (currentDestination !in listOf("login", "quiz/{focusId}")) {
+                BottomBar(navController = navController)
+            }
+        }
     ) {
-
         Box(modifier = Modifier.padding(it)) {
             AppNavGraph(navController = navController)
         }
