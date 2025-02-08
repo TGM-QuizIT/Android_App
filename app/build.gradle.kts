0f2bd3784aef20ee8d51e0b3f56e393e4c51c9bc
marcelturetschek <113130348+marcelturetschek@users.noreply.github.com>
2025-02-08 21:30:14 +0100

    CoroutineWorker Implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.support.annotations)
     implementation(libs.androidx.annotation)
+    implementation(libs.androidx.work.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/quizit_android_app/QuizITApplication.kt
+++ app/src/main/java/com/example/quizit_android_app/QuizITApplication.kt
@@ -1,8 +1,21 @@
 package com.example.quizit_android_app
 
 import android.app.Application
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.quizit_android_app.workers.DataSyncWorker
 import dagger.hilt.android.HiltAndroidApp
+import java.util.concurrent.TimeUnit
 
 @HiltAndroidApp
 class QuizITApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setupPeriodicWork()
+    }
+
+    private fun setupPeriodicWork() {
+        val syncWorkRequest = PeriodicWorkRequestBuilder<DataSyncWorker>(15, TimeUnit.MINUTES).build()
+        WorkManager.getInstance(this).enqueue(syncWorkRequest)
+    }
 }
