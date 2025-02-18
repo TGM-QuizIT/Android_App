79e153905dac2cd92aa08742430fdc188c8f0a40
marcelturetschek <113130348+marcelturetschek@users.noreply.github.com>
2025-02-18 10:44:35 +0100

    sync local pending and accepted friends data in AddFriendshipUseCase.kt DeleteDeclineFriendshipUseCase.kt

--- app/src/main/java/com/example/quizit_android_app/usecases/friendship/AddFriendshipUseCase.kt
+++ app/src/main/java/com/example/quizit_android_app/usecases/friendship/AddFriendshipUseCase.kt
@@ -2,12 +2,21 @@ package com.example.quizit_android_app.usecases.friendship
 
 import com.example.quizit_android_app.model.retrofit.DataRepo
 import com.example.quizit_android_app.model.retrofit.FriendshipResponse
+import com.example.quizit_android_app.usecases.localdata.friendship.SyncLocalAcceptedFriendsUseCase
+import com.example.quizit_android_app.usecases.localdata.friendship.SyncLocalPendingFriendsUseCase
 import javax.inject.Inject
 
 class AddFriendshipUseCase @Inject constructor(
-    val dataRepo: DataRepo
+    val dataRepo: DataRepo,
+    val syncLocalAcceptedFriendsUseCase: SyncLocalAcceptedFriendsUseCase,
+    val syncLocalPendingFriendsUseCase: SyncLocalPendingFriendsUseCase
 ) {
     suspend operator fun invoke(friendId: Int): FriendshipResponse {
-        return dataRepo.addFriendship(friendId)
+        val response = dataRepo.addFriendship(friendId)
+        if (response.status == "Success") {
+            syncLocalPendingFriendsUseCase()
+            syncLocalAcceptedFriendsUseCase()
+        }
+        return response
     }
 }
--- app/src/main/java/com/example/quizit_android_app/usecases/friendship/DeleteDeclineFriendshipUseCase.kt
+++ app/src/main/java/com/example/quizit_android_app/usecases/friendship/DeleteDeclineFriendshipUseCase.kt
@@ -2,17 +2,20 @@ package com.example.quizit_android_app.usecases.friendship
 
 import com.example.quizit_android_app.model.retrofit.DataRepo
 import com.example.quizit_android_app.model.retrofit.StatusResponse
+import com.example.quizit_android_app.usecases.localdata.friendship.SyncLocalAcceptedFriendsUseCase
 import com.example.quizit_android_app.usecases.localdata.friendship.SyncLocalPendingFriendsUseCase
 import javax.inject.Inject
 
 class DeleteDeclineFriendshipUseCase @Inject constructor(
     val dataRepo: DataRepo,
-    val syncLocalPendingFriendsUseCase: SyncLocalPendingFriendsUseCase
+    val syncLocalPendingFriendsUseCase: SyncLocalPendingFriendsUseCase,
+    val syncLocalAcceptedFriendsUseCase: SyncLocalAcceptedFriendsUseCase
 ) {
     suspend operator fun invoke(friendshipId: Int): StatusResponse {
         val response = dataRepo.deleteFriendship(friendshipId)
         if (response.status != null) {
             syncLocalPendingFriendsUseCase()
+            syncLocalAcceptedFriendsUseCase()
         }
         return response
     }
