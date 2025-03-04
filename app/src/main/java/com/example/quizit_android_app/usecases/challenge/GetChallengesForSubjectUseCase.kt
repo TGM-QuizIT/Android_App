3e51a42d93f0f3dde24cc0284b93b00cd98c1a18
marcelturetschek <113130348+marcelturetschek@users.noreply.github.com>
2025-03-04 12:18:08 +0100

    get challenges usecase also with focusId in mind

--- app/src/main/java/com/example/quizit_android_app/usecases/challenge/GetChallengesForSubjectUseCase.kt
+++ app/src/main/java/com/example/quizit_android_app/usecases/challenge/GetChallengesForSubjectUseCase.kt
@@ -2,12 +2,24 @@ package com.example.quizit_android_app.usecases.challenge
 
 import com.example.quizit_android_app.model.retrofit.ChallengeResponse
 import com.example.quizit_android_app.model.retrofit.DataRepo
+import com.example.quizit_android_app.usecases.focus.GetFocusForSubjectUseCase
+import com.example.quizit_android_app.usecases.subjects.GetAllSubjectsUseCase
 import javax.inject.Inject
 
 class GetChallengesForSubjectUseCase @Inject constructor(
-    val dataRepo: DataRepo
+    val dataRepo: DataRepo,
 ) {
-    suspend operator fun invoke(subjectId: Int): ChallengeResponse {
-        return dataRepo.getChallengesForSubject(subjectId)
+    suspend operator fun invoke(subjectId: Int? = null, focusId: Int? = null): ChallengeResponse {
+        return if (subjectId != null) {
+            dataRepo.getChallengesForSubject(subjectId)
+        } else if (focusId != null) {
+            val focuses = dataRepo.fetchAllFocusOfUser()
+            val subject = focuses.focus.find { it.focusId == focusId }?.subjectId
+            dataRepo.getChallengesForSubject(subject!!)
+        } else {
+            ChallengeResponse()
+        }
+
+
     }
 }
