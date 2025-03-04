417effb1e43ce497a48786439a6eaabaab8dedf4
marcelturetschek <113130348+marcelturetschek@users.noreply.github.com>
2025-03-04 11:50:31 +0100

    lebron goat

--- app/src/main/java/com/example/quizit_android_app/usecases/result/GetResultsUserUseCase.kt
+++ app/src/main/java/com/example/quizit_android_app/usecases/result/GetResultsUserUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.quizit_android_app.usecases.result
 
+import android.util.Log
 import com.example.quizit_android_app.model.ContentDataStore
 import com.example.quizit_android_app.model.retrofit.DataRepo
 import com.example.quizit_android_app.model.retrofit.Result
@@ -10,10 +11,11 @@ class GetResultsUserUseCase @Inject constructor(
     val contentDataStore: ContentDataStore
 ){
     suspend operator fun invoke(amount: Int? = null) : List<Result> {
-        val localData = contentDataStore.getResults().sortedBy { it.resultDateTime }
+        val localData = contentDataStore.getResults().sortedByDescending { it.resultDateTime }
+        Log.d("GetResultsUserUseCase", "invoke: $localData")
         return localData.ifEmpty {
             val remoteData = dataRepo.fetchResultsOfUser(amount)
-            contentDataStore.saveResults(remoteData.sortedBy { it.resultDateTime })
+            contentDataStore.saveResults(remoteData.sortedByDescending { it.resultDateTime })
             remoteData
         }
     }
